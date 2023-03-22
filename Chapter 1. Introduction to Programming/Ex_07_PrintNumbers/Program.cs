9b42ee818c231d6bb64bb2c5dd46ccc47b48c4ed
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-03-22 13:44:37 +0300

    Chapter 1. Introduction to Programming

---
+++ Chapter 1. Introduction to Programming/Ex_07_PrintNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex_07_PrintNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercice 7 - Write a program that prints the following numbers on the console 1, 101, 1001, each on a new line.
+
+            // Print the number 1
+            Console.WriteLine(1);
+            // Print the number 101
+            Console.WriteLine(101);
+            // Print the number 1001
+            Console.WriteLine(1001);
+        }
+    }
+}
