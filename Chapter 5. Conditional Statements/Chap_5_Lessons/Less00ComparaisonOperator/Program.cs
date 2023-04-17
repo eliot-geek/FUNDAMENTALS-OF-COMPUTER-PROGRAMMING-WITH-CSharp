cc222b7dece57fc8b4195bc99329b1f58a2024f9
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-04-17 22:21:37 +0300

    Chapter 5. Conditional Statements

---
+++ Chapter 5. Conditional Statements/Chap_5_Lessons/Less00ComparaisonOperator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Less00ComparaisonOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Operator Action
+                == Equal to
+                != Not equal to
+                >  Greater than
+                >= Greater than or equal to
+                <  Less than
+                <= Less than or equal to 
+
+            In C# several types of data that can be compared:
+            - numbers (int, long, float, double, ushort, decimal, …)
+            - characters (char)
+            - Booleans (bool)
+            - References to objects, also known as object pointers (string, object, arrays and others)      */
+
+            int weight = 700;                               // True
+            Console.WriteLine(weight >= 500);               // False
+            char gender = 'm';                              
+            Console.WriteLine(gender <= 'f');               // True
+            double colorWaveLength = 1.630;                 
+            Console.WriteLine(colorWaveLength > 1.621);     // True
+            int a = 5;
+            int b = 7;
+            bool condition = (b > a) && (a + b < a * b); 
+            Console.WriteLine(condition);                   // True
+            // characters are compared by their lexicographical order (the operation uses the Unicode numbers for the corresponding characters)
+            Console.WriteLine('B' == 'A' + 1);              // True
+
+            // Comparaison of Integers and Characters
+            // When comparing integers and characters, we directly compare their binary representation in memory i.e. we compare their values
+            
+            Console.WriteLine("\nchar 'a' == 'a'? " + ('a' == 'a'));  // True
+            Console.WriteLine("char 'a' == 'b'? " + ('a' == 'b'));    // False
+            Console.WriteLine("5 != 6? " + (5 != 6));                 // True
+            Console.WriteLine("5.0 == 5L? " + (5.0 == 5L));           // True
+            Console.WriteLine("true == false? " + (true == false));   // False
+
+            /* Comparaison of References to Objects
+            There are reference data types that do not contain their value(unlike the value types), but contain the address of the memory in the heap where their value is located.
+            Strings, arrays and classes are such types. They behave like a pointer to some value and can have the value null, i.e.no value.
+            When comparing reference type variables, we compare the addresses they hold, i.e.we check whether they point to the same location in the memory, i.e.to the same object.*/
+            string str = "beer";
+            string anotherStr = str; 
+
+            // == does not compare the content of the objects but rather checks if they point at the same location in memory, i.e. if they are one and the same object.
+            // The size comparisons (<, >, <= and >=) are not applicable for object type variables.
+            string thirdStr = "bee";
+            thirdStr = thirdStr + 'r';
+            Console.WriteLine("\nstr = {0}", str);
+            Console.WriteLine("anotherStr = {0}", anotherStr);
+            Console.WriteLine("thirdStr = {0}", thirdStr);
+            Console.WriteLine(str == anotherStr);                   // True - same object
+            Console.WriteLine(str == thirdStr);                     // True - equal objects
+            Console.WriteLine((object)str == (object)anotherStr);   // True  // The variable anotherStr is also reference type and gets the address (the reference) of str, i.e. points to the existing object str.
+            Console.WriteLine((object)str == (object)thirdStr);     // False // The two objects str and thirdStr have equal values, but are different objects, located at separate addresses in the memory.
+            Console.ReadKey();
+            // The operator == compares the strings by value and not by address. However, if we convert the three variables to objects and then compare them,
+            // we will get a comparison of the addresses in the heap where their values are located and the result will be different.
+            // Operator == has a special behavior when comparing strings, but for the rest of the reference types (like arrays or classes) it applies comparison by address.
+
+            // Logical Operators are: &&, ||, ! and ^
+            // The logical operators && (logical AND) and || (logical OR) are only used on Boolean expressions (values of type bool).
+            // && is also called short-circuit logical operator "and" because it does not lose time in additional unnecessary calculations 
+            // if the first operand is false, it does not lose time for evaluating the second operand it’s not possible the end result to be "true" when the first operand is not "true".
+            // || also called short-circuit logical operator "or" for the same reason
+            bool result = (2 < 3) && (3 < 4);
+            bool resul = (2 < 3) || (1 == 2);
+            Console.WriteLine(result);          // True
+            Console.WriteLine(resul);           // True
+
+            // Logical Operators & and | - both operands are calculated one after the other, although the final result is known in advance.
+            // That’s why these comparison operators are also known as full-circuit logical operators and are used very rarely.
+
+            // Logical Operators ^ and ! - The ^ operator, also known as exclusive OR (XOR), belongs to the full-circuit operators, because both operands are
+            // calculated one after the other. The result of applying the operator is true if exactly one of the operands is true, but not both simultaneously.
+            Console.WriteLine("Exclusive OR: " + ((2 < 3) ^ (4 > 3)));      // False
+            // The operator ! returns the reversed value of the Boolean expression to which it is attached
+            bool value = !(7 == 5);
+            Console.WriteLine(value);           // True
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter 5. Conditional Statements/Chap_5_Lessons/Less01IfElseStatement/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace Less01IfElseStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conditional Statements "if" and "if-else"
+            // Conditional Statement "if"
+            Console.WriteLine("Enter two numbers for comparaison");
+            Console.Write("Enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int biggerNumber = firstNumber;
+            if (secondNumber > firstNumber)
+            {
+                biggerNumber = secondNumber;
+            }
+            Console.WriteLine("The bigger number is : {0}", biggerNumber);
+
+            // Bad practice: misleading code
+            int a = 6;
+            if(a > 5)
+                Console.WriteLine("\nThe variable is greater than 5.");
+            Console.WriteLine("This code will always execute!");
+
+
+            // Conditional Statement "if-else"
+            int x = 2;
+            if (x > 3)
+            {
+                Console.WriteLine("\nx is greater than 3");
+            }
+            else
+            {
+                Console.WriteLine("\nx is not greater than 3");
+            }
+
+
+            // Nested if else statements
+            int first = 5;
+            int second = 3;
+            if (first == second)
+            {
+                Console.WriteLine("\nThese two numbers are equal.");
+            }
+            else
+            {
+                if (first > second)
+                {
+                    Console.WriteLine("\nThe first number is greater.");
+                }
+                else
+                {
+                    Console.WriteLine("\nThe second number is greater.");
+                }
+            }
+            Console.WriteLine();
+
+            /* Conditional "if" Statements – Good Practices - Here are some guidelines, which we recommend for writing if, structures: 
+            - Use blocks, surrounded by curly brackets {} after if and else in order to avoid ambiguity
+            - Always format the code correctly by offsetting it with one tab inwards after if and else, for readability and avoiding ambiguity.
+            - Prefer switch-case structure to of a series of if-else-if-else-… structures or nested if-else statement, if possible.   */
+
+            // Sequences of if-else-if-else
+            char ch = 'X';
+            if (ch == 'A' || ch == 'a')
+            {
+                Console.WriteLine("Vowel [ei]");
+            }
+            else if (ch == 'E' || ch == 'e')
+            {
+                Console.WriteLine("Vowel [i:]");
+            }
+            else if (ch == 'I' || ch == 'i')
+            {
+                Console.WriteLine("Vowel [ai]");
+            }
+            else if (ch == 'O' || ch == 'o')
+            {
+                Console.WriteLine("Vowel [ou]");
+            }
+            else if (ch == 'U' || ch == 'u')
+            {
+                Console.WriteLine("Vowel [ju:]");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+            Console.WriteLine();
+
+
+            // Conditional Statement "Switch-case"
+            // The switch statement is a clear way to implement selection among many options. It requires a selector, which is calculated to a certain value.
+            // The selector type could be an integer number, char, string or enum.
+            // If we want to use for example an array or a float as a selector, it will not work. For non-integer data types, we should use a series of if statements.
+            int numb = 6;
+            switch (numb)
+            {
+                case 1:
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                    Console.WriteLine("The number is not prime");
+                    break;
+                case 2:
+                case 3:
+                case 5:
+                case 7:
+                    Console.WriteLine("The number is prime");
+                    break;
+                default:
+                    Console.WriteLine("Unknown number!");
+                    break;
+            }
+            Console.ReadLine();
+
+            /* Good Practices When Using "switch-case"
+            - A good practice when using the switch statement is to put the default statement at the end, in order to have easier to read code.
+            - It’s good to place first the cases, which handle the most common situations. Case statements, which handle situations occurring rarely, 
+            can be placed at the end of the structure.
+            - If the values in the case labels are integer, it’s recommended that they be arranged in ascending order.
+            - If the values in the case labels are of character type, it’s recommended that the case labels are sorted alphabetically.
+            - It’s advisable to always use a default block to handle situations that cannot be processed in the normal operation of the program. 
+            If in the normal operation of the program the default block should not be reachable, you could put in it a code reporting an error. */
+        }
+    }
+}
