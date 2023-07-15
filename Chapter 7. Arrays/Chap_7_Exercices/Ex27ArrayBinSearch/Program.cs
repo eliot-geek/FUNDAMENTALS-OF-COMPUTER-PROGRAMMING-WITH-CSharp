741ce13b8533638abae366535e1562c74b07f0f5
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-07-16 02:12:42 +0300

    Chapter 7. Arrays

---
+++ Chapter 7. Arrays/Chap_7_Exercices/Ex27ArrayBinSearch/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Ex27ArrayBinSearch
+{
+    class Array
+    {
+        public static int? BinSearch(int[] arr, int k)
+        {
+            int? l = int.MinValue;
+
+            // Find the largest number in array which is ≤ k
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] <= k) l = arr[i];
+                else break;
+            }
+
+            // Returns the largest number
+            return (l == int.MinValue) ? null : l;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 27
+            /*: Write a program, that reads from the console an array of N integers and an integer K, sorts the array and using the method Array.BinSearch() 
+             * finds the largest number in the array which is ≤ K. */
+            #endregion
+
+            // Creates some array
+            Console.Write("Please, enter the number of elements in array: ");
+            int[] array = new int[int.Parse(Console.ReadLine())];
+
+            // Fill the array with numbers
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" arr[{0}] = ", i);
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Reads some integer number K
+            Console.Write("\nPlease, enter some integer number: K = ");
+            int K = int.Parse(Console.ReadLine());
+
+            // Sorts the array
+            System.Array.Sort(array);
+
+            // Prints the result from Array.BinSearch()
+            dynamic largest = Array.BinSearch(array, K);
+            Console.Write("\nThe largest number in the array which is <= K is: ");
+            Console.WriteLine(largest ?? "There isn't such number!");
+        }
+    }
+}
---
+++ Chapter 7. Arrays/Chap_7_Exercices/Ex28SortByLength/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Ex28SortByLength
+{
+    internal class Program
+    {
+        // Sort some array of strings
+        private static void Sort(string[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                // Compare each two elements in the array
+                if (arr[i].Length < arr[i - 1].Length)
+                {
+                    // Keep the value of the first element
+                    string temp = arr[i - 1];
+
+                    // Swap the elements
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+
+                    // Reset the loop
+                    i = 0;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            #region Task 28
+            // You are given an array of strings. Write a method that sorts the array by the length of its elements (the number of characters composing them).
+            #endregion
+
+            // Some given array of strings
+            string[] strings = new string[] { "shark", "elephant", "ball", "tower", "run", "point" };
+
+            Console.WriteLine("The given array of strings:");
+            foreach (var element in strings) 
+                Console.Write(element + " ");
+            Console.WriteLine("\n");
+
+            // First variant: sorting by loop
+            Console.WriteLine("Variant 1: Sorting by loop:\n");
+            Sort(strings);
+            foreach (var item in strings) 
+                Console.WriteLine(item);
+
+            // Second variant: sorting by LINQ
+            Console.WriteLine("\nVariant 2: Sorting by LINQ:\n");
+            var sorted = from n in strings orderby n.Length ascending select n;
+            foreach (var item in sorted) 
+                Console.WriteLine(item);
+
+            // Third variant: sorting by Lambda
+            Console.WriteLine("\nVariant 3: Sorting by Lambda:\n");
+            foreach (var item in strings.OrderBy(m => m.Length)) 
+                Console.WriteLine(item);
+            Console.WriteLine();
+
+        }
+    }
+}
