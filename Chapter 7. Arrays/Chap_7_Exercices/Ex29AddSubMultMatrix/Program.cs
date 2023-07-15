4f5d4ea357e7630d6dd685a654f1b1dc4f378beb
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-07-16 02:13:44 +0300

    Chapter 7. Arrays

---
+++ Chapter 7. Arrays/Chap_7_Exercices/Ex29AddSubMultMatrix/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.ComponentModel;
+
+namespace Ex29AddSubMultMatrix
+{
+    class Matrix
+    {
+        // Fields
+        int rows, columns;
+        int[,] matrix;
+
+        // Constructor
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            matrix = new int[rows, columns];
+        }
+
+        // Indexer
+        public int this[int r, int c]
+        {
+            get { return matrix[r, c]; }
+            set { matrix[r, c] = value; }
+        }
+
+        // Operators
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            // Adding of matrices of the same size
+            return Operation(m1, m2, true);
+        }
+
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            // Subtracting of matrices of the same size
+            return Operation(m1, m2, false);
+        }
+
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            // Multiplying of matrices of the same size
+            Matrix m = new Matrix(m1.rows, m1.columns);
+            for (int r = 0; r < m.rows; r++)
+            {
+                for (int c = 0; c < m.columns; c++)
+                {
+                    for (int k = 0; k < m.columns; k++)
+                    {
+                        m[r, c] += (dynamic)m1[r, k] * m2[k, c];
+                    }
+                }
+            }
+            return m;
+        }
+
+        // Method for calcualtion of adding and subtracting
+        private static Matrix Operation(Matrix m1, Matrix m2, bool isAdd)
+        {
+            Matrix m = new Matrix(m1.rows, m1.columns);
+            for (int r = 0; r < m.rows; r++)
+            {
+                for (int c = 0; c < m.columns; c++)
+                {
+                    // For adding
+                    if (isAdd) m[r, c] = (dynamic)m1[r, c] + m2[r, c];
+
+                    // For subtracting
+                    else m[r, c] = (dynamic)m1[r, c] - m2[r, c];
+                }
+            }
+            return m;
+        }
+
+        // String output for this class
+        public override string ToString()
+        {
+            string result = String.Empty;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result += String.Format("{0,11:F2}", matrix[i, j]);
+                }
+                result += "\n";
+            }
+            return result.ToString();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 29
+            // Write a class Matrix, to holds a matrix of integers. Overload the operators for adding, subtracting and multiplying of matrices, indexer for accessing the matrix content and ToString().
+            #endregion
+
+            // Creates two matrices with the same size
+            int size = 4;
+            Matrix matrix1 = new Matrix(size, size);
+            Matrix matrix2 = new Matrix(size, size);
+
+            // Generate values for the matrices by random generator
+            Random Generator = new Random();
+            for (int r = 0; r < size; r++)
+            {
+                for (int c = 0; c < size; c++)
+                {
+                    matrix1[r, c] = Generator.Next(-10, 10);
+                    matrix2[r, c] = Generator.Next(-10, 10);
+                }
+            }
+
+            // Print the result
+            Console.WriteLine("Matrix 1\n" + matrix1);
+            Console.WriteLine("Matrix 2\n" + matrix2);
+            Console.WriteLine("Matrix 1 + Matrix 2\n" + (matrix1 + matrix2));
+            Console.WriteLine("Matrix 1 - Matrix 2\n" + (matrix1 - matrix2));
+            Console.WriteLine("Matrix 1 * Matrix 2\n" + (matrix1 * matrix2));
+        }
+    }
+}
---
+++ Chapter 7. Arrays/Chap_7_Exercices/Ex30LargestArea/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Ex30LargestArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 25
+            /* Write a program, which finds in a given matrix the largest area of equal numbers. We define an area in the matrix as a set of neighbor cells 
+            (by row and column). Here is one example with an area containing 13 elements with equal value of 3: 
+            
+            1  3  2  2  2  4
+            3  3  3  2  4  4 
+            4  3  1  2  3  3    ====> 13
+            4  3  1  3  3  1
+            4  3  3  3  1  1
+
+            Guide: This is a little bit more difficult. You can use different graph traversal algorithms like "DFS" (Depth-First-Search) and "BFS" 
+            (Breadth-First-Search) to go through all the cells in certain area starting from any cell that belongs to it. If you have an area traversal 
+            algorithm (like DFS), run it several times starting from unvisited cell and mark the cells of the traversed area as visited. Repeat this until 
+            all cells become visited. Read later in this book about DFS and BFS in the chapter “Trees and Graphs” or find information about these algorithms 
+            in Internet. */
+            #endregion
+
+            // Given matrix of elements
+            int[,] m = {
+                { 1, 3 ,2 ,2 ,2, 4 },
+                { 3, 3 ,3 ,2 ,4, 4 },
+                { 4, 3 ,1 ,2 ,3, 3 },
+                { 4, 3 ,1 ,3 ,3, 1 },
+                { 4, 3 ,3 ,3 ,1, 1 }
+                              };
+
+            // Finds the largest area of equal neighbor elements
+            int maxPath = 0;
+            for (int r = 0; r < m.GetLength(0); r++)
+            {
+                for (int c = 0; c < m.GetLength(1); c++)
+                {
+                    int count = 1;
+                    bool[,] tempM = new bool[m.GetLength(0), m.GetLength(1)];
+                    tempM[r, c] = true;
+                    FindPath(m, m[r, c], tempM, r, c, ref count);
+                    if (count > maxPath) maxPath = count;
+                }
+            }
+            Console.WriteLine(maxPath);
+        }
+
+        // Find the longest path for some element
+        private static void FindPath(int[,] m, int n, bool[,] tempM, int r, int c, ref int count)
+        {
+            if (r > 0 && tempM[r - 1, c] == false && m[r - 1, c] == n) count = Count(m, n, tempM, count, r - 1, c);
+            if (r < m.GetLength(0) - 1 && tempM[r + 1, c] == false && m[r + 1, c] == n) count = Count(m, n, tempM, count, r + 1, c);
+            if (c > 0 && tempM[r, c - 1] == false && m[r, c - 1] == n) count = Count(m, n, tempM, count, r, c - 1);
+            if (c < m.GetLength(1) - 1 && tempM[r, c + 1] == false && m[r, c + 1] == n) count = Count(m, n, tempM, count, r, c + 1);
+        }
+
+        // Count the elements included in the path
+        private static int Count(int[,] m, int n, bool[,] tempM, int count, int nr, int nc)
+        {
+            tempM[nr, nc] = true;
+            count++;
+            FindPath(m, n, tempM, nr, nc, ref count);
+            return count;
+        }
+    }
+}
