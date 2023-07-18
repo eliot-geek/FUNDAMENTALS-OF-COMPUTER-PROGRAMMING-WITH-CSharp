16e5c596622c3f903df6d319a75f314405659019
Eliot <45435800+eliot-geek@users.noreply.github.com>
2023-07-18 22:35:36 +0300

    Exams

---
+++ Exams (From Chap 1 To Chap 7)/ArithmeticCalc/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace ArithmeticCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Arithmetic Calculations (Switch)
+            /* This C# program reads three input values from the user: a double a, an integer b, and another double c. 
+             * Based on the value of b, the program performs specific calculations and outputs the results r and k.
+            
+            Read three input values: a, b, and c, from the user.
+            Initialize two double variables, r and k, to store the results of the calculations.
+            Use a switch statement to determine the calculation based on the value of b:
+            If b is 3, calculate r as the sum of a and c.
+            If b is 6, calculate r as the product of a and c.
+            If b is 9, calculate r as the remainder of a divided by c.
+            For any other value of b, do nothing (default case).
+            Assign the value of r to the variable k.
+            Check if r is divisible by 3:
+            If yes, divide r by 3 and store the result back in r.
+            If no, calculate the remainder of r divided by 3 and store the result back in r.
+            Output the final values of r and k to the console.*/
+            #endregion
+
+            // Read input values from the user
+            Console.Write("Enter a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            // Initialize variables to store the results
+            double r = 0;
+            double k = 0;
+
+            // Perform calculations based on the value of 'b'
+            switch (b)
+            {
+                case 3:
+                    r = a + c;  // If 'b' is 3, calculate 'r' as the sum of 'a' and 'c'
+                    break;
+                case 6:
+                    r = a * c;  // If 'b' is 6, calculate 'r' as the product of 'a' and 'c'
+                    break;
+                case 9:
+                    r = a % c;  // If 'b' is 9, calculate 'r' as the remainder of 'a' divided by 'c'
+                    break;
+                default:
+                    break;  // For any other value of 'b', do nothing
+            }
+
+            // Assign the value of 'r' to variable 'k'
+            k = r;
+
+            // Check if 'r' is divisible by 3
+            if ((r % 3) == 0)
+            {
+                r = r / 3;  // If 'r' is divisible by 3, divide 'r' by 3 and store the result back in 'r'
+            }
+            else
+            {
+                r = r % 3;  // If 'r' is not divisible by 3, calculate the remainder of 'r' divided by 3 and store the result back in 'r'
+            }
+
+            // Output the final values of 'r' and 'k'
+            Console.WriteLine("Final r: " + r);
+            Console.WriteLine("k: " + k);
+        }
+    }
+}
---
+++ Exams (From Chap 1 To Chap 7)/CharSummationCounting/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace CharSummationCounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task: Character Summation and Counting
+            /* 
+            Task: Character Summation and Counting
+            Conditions:
+            The program will read a string x from the user.
+            It will initialize three variables sum, cnt, and i to 0.
+            The program will loop through each character in the input string x.
+            During the loop:
+            a. If the character's position is an even index (0-based) and it is a digit, the program will:
+            Increment the cnt variable by 1, to count the number of even-positioned digits.
+            Add the numeric value of the digit to the sum variable.
+            b. Increment the i variable to keep track of the current character's position.
+            After the loop, the program will print the values of cnt and sum separated by a space to the console.*/
+            #endregion
+
+
+            Console.Write("Enter a string of numbers: ");
+            string x = Console.ReadLine();
+
+            // Initialize variables to store the sum and count of even-positioned digits
+            double sum = 0, count = 0, i = 0;
+
+            // Loop through each character in the input string 'x'
+            foreach (var ch in x)
+            {
+                // Check if the current character's position is even (0-based) and it is a digit
+                if ((i % 2 == 0) && char.IsDigit(ch))
+                {
+                    // Increment 'cnt' by 1 to count the number of even-positioned digits
+                    count++;
+
+                    // Add the numeric value of the digit to 'sum'
+                    sum += (double.Parse(ch.ToString()) - double.Parse("0"));
+                }
+
+                // Increment 'i' to keep track of the current character's position
+                i++;
+            }
+
+            // Output the count and sum of even-positioned digits to the console
+            Console.WriteLine(count + " " + sum);
+        }
+    }
+}
